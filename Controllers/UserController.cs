da0f8e450c80f7368766cd9ee05e3ff9c3e5fc92
github_cloudlabsuser_1954 <github_cloudlabsuser_1954@cloudlabsaiuser.com>
2024-05-10 09:05:18 +0000

    The most significant changes in the code are the extensive modifications in the UserController.cs file and the adjustment of the bindingRedirect for the WebGrease library in the Web.config file.
    In the UserController.cs file, several methods have been implemented to handle HTTP requests, including Index, Details, Create, Edit, and Delete. These methods are crucial for managing operations on user data.
    
    In the Web.config file, the bindingRedirect for the WebGrease library has been modified. The oldVersion attribute has been changed to redirect any version of the WebGrease library from 0.0.0.0 to 1.6.5135.21930 to use version 1.6.5135.21930. This change ensures that the correct version of the library is used, which can prevent potential compatibility issues.
    
    List of changes:
    
    1. Addition of a reference to the Microsoft.CodeDom.Providers.DotNetCompilerPlatform library in the CRUD application 2.csproj file.
    2. Removal of the reference to the Microsoft.CodeDom.Providers.DotNetCompilerPlatform library in the CRUD application 2.csproj file.
    3. Removal of a comment at the end of the CRUD application 2.csproj file.
    4. Implementation of various methods in the UserController class in the UserController.cs file.
    5. Modification of the bindingRedirect for the WebGrease library in the Web.config file.

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -1,66 +1,135 @@
 using CRUD_application_2.Models;
 using System.Linq;
 using System.Web.Mvc;
- 
+
 namespace CRUD_application_2.Controllers
 {
     public class UserController : Controller
     {
         public static System.Collections.Generic.List<User> userlist = new System.Collections.Generic.List<User>();
+
+        public UserController()
+        {
+            if (userlist.Count == 0)
+            {
+                for (int i = 1; i <= 10; i++)
+                {
+                    userlist.Add(new User
+                    {
+                        Id = i,
+                        Name = $"User{i}",
+                        Email = $"user{i}@example.com",
+                    });
+                }
+            }
+        }
+
         // GET: User
+        [HttpGet]
         public ActionResult Index()
         {
-            // Implement the Index method here
+            return View(userlist);
         }
- 
+
         // GET: User/Details/5
+        [HttpGet]
         public ActionResult Details(int id)
         {
-            // Implement the details method here
+            var user = userlist.FirstOrDefault(u => u.Id == id);
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+            return View(user);
         }
- 
+
         // GET: User/Create
+        [HttpGet]
         public ActionResult Create()
         {
-            //Implement the Create method here
+            return View(new User());
         }
- 
-        // POST: User/Create
+
+        // GET: User/Create
         [HttpPost]
         public ActionResult Create(User user)
         {
-            // Implement the Create method (POST) here
+            if (ModelState.IsValid)
+            {
+                // Add the new user to the list
+                userlist.Add(user);
+
+                // Return a 201 Created status code
+                return new HttpStatusCodeResult(System.Net.HttpStatusCode.Created);
+            }
+
+            // If model state is not valid, return the user back to the Create view
+            return View(user);
         }
- 
+
+
         // GET: User/Edit/5
+        [HttpGet]
         public ActionResult Edit(int id)
         {
-            // This method is responsible for displaying the view to edit an existing user with the specified ID.
-            // It retrieves the user from the userlist based on the provided ID and passes it to the Edit view.
+            var user = userlist.FirstOrDefault(u => u.Id == id);
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+            return View(user);
         }
- 
+
         // POST: User/Edit/5
         [HttpPost]
         public ActionResult Edit(int id, User user)
         {
-            // This method is responsible for handling the HTTP POST request to update an existing user with the specified ID.
-            // It receives user input from the form submission and updates the corresponding user's information in the userlist.
-            // If successful, it redirects to the Index action to display the updated list of users.
-            // If no user is found with the provided ID, it returns a HttpNotFoundResult.
-            // If an error occurs during the process, it returns the Edit view to display any validation errors.
+            var existingUser = userlist.FirstOrDefault(u => u.Id == id);
+            if (existingUser == null)
+            {
+                return HttpNotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                existingUser.Name = user.Name;
+                existingUser.Email = user.Email;
+
+                return RedirectToAction("Index");
+            }
+
+            // If model state is not valid, return the user back to the Edit view
+            return View(user);
         }
- 
+
+
         // GET: User/Delete/5
         public ActionResult Delete(int id)
         {
-            // Implement the Delete method here
+            var user = userlist.FirstOrDefault(u => u.Id == id);
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+
+            userlist.Remove(user);
+            return RedirectToAction("Index");
         }
- 
+
+
         // POST: User/Delete/5
         [HttpPost]
         public ActionResult Delete(int id, FormCollection collection)
         {
-            // Implement the Delete method (POST) here
+            var user = userlist.FirstOrDefault(u => u.Id == id);
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+
+            userlist.Remove(user);
+            return RedirectToAction("Index");
         }
+
     }
 }
